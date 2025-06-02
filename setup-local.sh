1c54b150840a3d343458971f3bceced49533b8ff
NoRelect <info@norelect.ch>
2025-06-02 21:37:34 +0000

    chore: Bump kind version

--- setup-local.sh
+++ setup-local.sh
@@ -5,7 +5,7 @@ set -e
 # Install kind
 if ! type "kind" > /dev/null; then
     echo "Installing kind"
-    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.24.0/kind-linux-amd64
+    curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.29.0/kind-linux-amd64
     chmod +x ./kind
     sudo mv ./kind /usr/local/bin/kind
 fi
