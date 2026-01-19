df097c654c16f878894b234184e96ff3e260a201
Carlo Field <carlo@swiss.dev>
2026-01-19 07:29:01 +0100

    fix: reconciliation smells
    Signed-off-by: Carlo Field <carlo@swiss.dev>

--- modules/cluster-bootstrap/namespaces.tf
+++ modules/cluster-bootstrap/namespaces.tf
@@ -1,6 +1,11 @@
 resource "kubernetes_namespace_v1" "infra_argocd" {
   metadata {
     name = "infra-argocd"
+    labels = {
+      "app.kubernetes.io/name"      = "infra-argocd"
+      "app.kubernetes.io/component" = "infra"
+      "app.kubernetes.io/part-of"   = "infra"
+    }
   }
 }
 
@@ -15,6 +20,11 @@ resource "kubernetes_namespace_v1" "infra_authentik" {
   lifecycle { enabled = var.authentik.enabled }
   metadata {
     name = "infra-authentik"
+    labels = {
+      "app.kubernetes.io/component" = "infra"
+      "app.kubernetes.io/name"      = "infra-authentik"
+      "app.kubernetes.io/part-of"   = "infra"
+    }
   }
 }
 
@@ -22,6 +32,11 @@ resource "kubernetes_namespace_v1" "berg" {
   lifecycle { enabled = var.berg.enabled }
   metadata {
     name = "berg"
+    labels = {
+      "app.kubernetes.io/name"      = "berg"
+      "app.kubernetes.io/component" = "berg"
+      "app.kubernetes.io/part-of"   = "berg"
+    }
   }
 }
 
@@ -36,6 +51,11 @@ resource "kubernetes_namespace_v1" "infra_cert_manager" {
   lifecycle { enabled = var.cert_manager.enabled }
   metadata {
     name = "infra-cert-manager"
+    labels = {
+      "app.kubernetes.io/name"      = "infra-cert-manager"
+      "app.kubernetes.io/component" = "infra"
+      "app.kubernetes.io/part-of"   = "infra"
+    }
   }
 }
 
@@ -43,6 +63,11 @@ resource "kubernetes_namespace_v1" "infra_external_dns" {
   lifecycle { enabled = var.external_dns.enabled }
   metadata {
     name = "infra-external-dns"
+    labels = {
+      "app.kubernetes.io/name"      = "infra-external-dns"
+      "app.kubernetes.io/component" = "infra"
+      "app.kubernetes.io/part-of"   = "infra"
+    }
   }
 }
 
