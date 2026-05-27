586917db5f5054b6731252bd133bd051467004c1
NoRelect <info@norelect.ch>
2026-05-28 01:09:54 +0200

    chore(utilities): update solve migration script to latest schema

--- utilities/migrate-solves.sql
+++ utilities/migrate-solves.sql
@@ -25,7 +25,7 @@ END;
 SELECT 'INSERT INTO "Players" ("Id", "Name", "FederatedId", "CreatedAt", "Email") SELECT '''||get_remapped_player_id("FederatedId")||''','''||"Name"||''','''||"FederatedId"||''','''||"CreatedAt"||''','''||"Email"||''' WHERE NOT EXISTS (SELECT NULL FROM "Players" WHERE "FederatedId" = '''||"FederatedId"||''');' FROM "Players";
 
 -- Migrate solves based on federated id.
-SELECT 'INSERT INTO "Solves" ("Id", "SolvedAt", "PlayerId", "ChallengeId") VALUES ('''||"Solves"."Id"||''','''||"Solves"."SolvedAt"||''',(SELECT "Id" FROM "Players" as "p" WHERE "p"."FederatedId" = '''||"Players"."FederatedId"||'''),'''||"Solves"."ChallengeId"||''');' FROM "Solves" JOIN "Players" ON "Solves"."PlayerId" = "Players"."Id";
+SELECT 'INSERT INTO "Solves" ("SolvedAt", "PlayerId", "ChallengeId") VALUES ('''||"Solves"."SolvedAt"||''',(SELECT "Id" FROM "Players" as "p" WHERE "p"."FederatedId" = '''||"Players"."FederatedId"||'''),'''||"Solves"."ChallengeId"||''');' FROM "Solves" JOIN "Players" ON "Solves"."PlayerId" = "Players"."Id";
 
 DROP FUNCTION get_remapped_player_id;
 DROP FUNCTION get_namespace_uuid;
