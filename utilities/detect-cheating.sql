5468ec9896a0fa8eb9da31064b7bc59f49074016
NoRelect <info@norelect.ch>
2026-05-28 00:37:26 +0200

    chore(utilities): update script to handle teams and also display usernames

--- utilities/detect-cheating.sql
+++ utilities/detect-cheating.sql
@@ -1,10 +1,15 @@
--- Detect cheating players by looking for invalid flag submissions that contain valid flags of other players
-select
-	i."ChallengeName" as "Challenge",
-	i."StartedAt" as "InstanceStartedAt",
-	i."TerminatedAt" as "InstanceTerminated",
-	s."SubmittedAt" as "SubmittedAt",
-	i."PlayerId" as "InstancePlayerId",
-	s."PlayerId" as "SubmitterPlayerId",
-from "Submissions" as s join "Instances" as i on s."Value" = i."DynamicFlag" where s."PlayerId" != i."PlayerId";
-
+-- Detect cheating players by looking for invalid flag submissions that contain valid flags of other players that are not on the same team
+SELECT
+    i."ChallengeName" AS "Challenge",
+    i."StartedAt" AS "InstanceStartedAt",
+    i."TerminatedAt" AS "InstanceTerminated",
+    s."SubmittedAt" AS "SubmittedAt",
+    i."PlayerId" AS "InstancePlayerId",
+    ip."Name" AS "InstancePlayerName",
+    s."PlayerId" AS "SubmitterPlayerId",
+    sp."Name" AS "SubmitterPlayerName"
+FROM "Submissions" AS s
+JOIN "Instances" AS i ON s."Value" = i."DynamicFlag"
+JOIN "Players" AS ip ON i."PlayerId" = ip."Id"
+JOIN "Players" AS sp ON s."PlayerId" = sp."Id"
+WHERE s."PlayerId" != i."PlayerId" AND (ip."TeamId" IS NULL OR sp."TeamId" IS NULL OR ip."TeamId" != sp."TeamId");
